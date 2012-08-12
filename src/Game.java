5d3a7ca9ffc12288aeb3f03a53faf712316f1a49
Timothy Hahn <timyhahn@gmail.com>
2012-08-11 23:34:40 -0400

    Some Basic Layout Started: 	* There exists a Menu. That Menu has it's options, but can only 	quit.
    	* There is a statistics bar with the time taken and the mines left
    
    	* The minesweeper board is there, with both floor and tiles, but no
    	numbers. Not sure how I'm going to do that yet.
    
    	* There is also a help section at the bottom with an insulting
    	* message

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+public class Game {
+
+	public static void main (String[] args){ 
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void  run() {
+				GameWindow gw = new GameWindow();
+				gw.setUpLayout();
+			}
+		});
+	}
+}
